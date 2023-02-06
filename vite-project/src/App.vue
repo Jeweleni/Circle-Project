1e9b6f4628bb812d197da5cf43638e3026b2bf0d
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-02-06 02:21:28 +0100

    1st commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Main firstName="Jewel" lastName="Eni" age="20" hobbies="codding, reading, listening to music"/>
+    <Main firstName="Chisom" lastName="Akubo" age="21" hobbies="sewing"/>
+    <Main firstName="Tamuno" lastName="Omubo" age="22" hobbies="reading"/>
+  </div>
+
+</template>
+
+<script>
+import Main from './components/Main.vue'
+export default {
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vite-project/src/components/Main.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h2>Hello World, lets us work on props</h2>
+        <h3>My name is {{ firstName }} {{ lastName }}. I am {{ age }} years old. </h3>
+        <h3>My hobbies are: {{ hobbies }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    props: {
+        firstName: {
+            type: String,
+            default: 'John',
+        },
+        lastName: {
+            type: String,
+            default: 'Doe',
+        },
+        age: {
+            type: Number,
+            default: 30,
+        },
+        hobbies: {
+            type: Array,
+            default: () => ['Reading', 'Coding', 'Swimming'],
+        },
+
+    },
+    };
+
+</script>
+
+<style scoped>
+
+.main {
+    background-color: #f5f5f5;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+</style>
